eddbdef3ea2740c4f9dfe1acc43a0c38bab98bf3
zealotzealot <hsh0908y@gmail.com>
2018-06-02 23:47:32 +0900

    Add decorator csrf_exempt and require_http_methods in views

--- orz/api/views.py
+++ orz/api/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from orz.api.models import *
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 
 
@@ -8,6 +10,8 @@ def temp_view(request):
     return JsonResponse({"text":"Hello, World!"})
 
 
+@csrf_exempt
+@require_http_methods(["GET"])
 def schools(request):
     if request.method == "GET":
         schools = School.objects.all()
@@ -15,6 +19,8 @@ def schools(request):
         return JsonResponse(ctx, safe=False)
 
 
+@csrf_exempt
+@require_http_methods(["GET"])
 def school(request, schoolID):
     if request.method == "GET":
         school = get_object_or_404(School, id=schoolID)
@@ -22,6 +28,8 @@ def school(request, schoolID):
         return JsonResponse(ctx)
 
 
+@csrf_exempt
+@require_http_methods(["GET"])
 def schoolCourses(request, schoolID):
     if request.method == "GET":
         school = get_object_or_404(School, id=schoolID)
