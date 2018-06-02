7468e4e58722c8e44561de1dee093c47359168a9
zealotzealot <hsh0908y@gmail.com>
2018-06-02 21:58:43 +0900

    Implement urls

--- orz/api/urls.py
+++ orz/api/urls.py
@@ -3,5 +3,60 @@ from . import views
 
 
 urlpatterns = [
-    path('some_url', views.temp_view),
+    # List of schools
+    path('schools', views.temp_view),
+
+    # A specific school
+    path('schools/<int:schoolID>', views.temp_view),
+
+    # List of courses in a specific school
+    path('schools/<int:schoolID>/courses', views.temp_view),
+
+
+
+    # A specific course
+    path('courses/<int:courseID>', views.temp_view),
+
+    # List of assignments of a specific course
+    path('courses/<int:courseID>/assignments', views.temp_view),
+
+
+
+    # A specific assignment
+    path('assignments/<int:assignmentID>', views.temp_view),
+
+
+
+    # A specific lecturer
+    path('lecturer/<int:lecturerID>', views.temp_view),
+
+    # List of courses managed by a specific lecturer
+    path('lecturer/<int:lecturerID>/courses', views.temp_view),
+
+
+
+    # A specific student
+    path('student/<int:studentID>', views.temp_view),
+
+    # List of courses subscribed by a specific student
+    path('student/<int:studentID>/courses', views.temp_view),
+
+    # List of assignments of a specific student
+    path('student/<int:studentID>/assignments', views.temp_view),
+
+    # A specific assignment of a specific student
+    path('student/<int:studentID>/assignments/<int:AssignmentID>', views.temp_view),
+
+    # List of personal schedules of a specific student
+    path('student/<int:studentID>/personal-schedules', views.temp_view),
+
+    # A specific personal schedule of a specific student
+    path('student/<int:studentID>/personal-schedules/<int:scheduleID>', views.temp_view),
+
+    # List of time for assignments of a specific student
+    path('student/<int:studentID>/time-for-assignments', views.temp_view),
+
+    # A specific time for assignment of a specific student
+    path('student/<int:studentID>/time-for-assignments/<int:scheduleID>', views.temp_view)
+
 ]
