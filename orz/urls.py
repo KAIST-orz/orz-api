663856e3f8613666a8cd7bcfaedadb7cd9affa68
zealotzealot <hsh0908y@gmail.com>
2018-06-06 20:13:31 +0900

    Added version in url

--- orz/urls.py
+++ orz/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('orz.api.urls')),
+    path('api/v1/', include('orz.api.urls')),
 ]
